b250a915180fa74510bfaffc1da6844588715e3a
mbardolia <manish@bardolia.com>
2019-10-08 15:26:05 -0400

    Updated sql to show all TP brokers
    Updated sql to show all TP brokers ids with TEvo names when it exists

--- compare_data/tickpick_brokers.sql
+++ compare_data/tickpick_brokers.sql
@@ -2,10 +2,11 @@ create table tickpick_brokers as
 select
   distinct
   tickpick.brokerage_id as id,
-  tevo.broker_name as name
+  max(tevo.broker_name) as name
 from
   tickpick
-  join tevo on tevo.remote_id = tickpick.remote_id
+  left join tevo on tevo.remote_id = tickpick.remote_id
+group by 1
 order by
   tickpick.brokerage_id
 
